a958923e54544aa74beb00c5c46bdd2f154bb6ae
Ryochi <ryo1.t.zubu@gmail.com>
2022-01-26 18:01:56 +1000

    Done

--- src/Hero.js
+++ src/Hero.js
@@ -4,29 +4,27 @@ import { useGlobalContext } from './context';
 
 const Hero = () => {
   const { closeSubmenu } = useGlobalContext();
-
-  return(
-    <section className="hero">
-      <div className="hero-center">
-        <article className="hero-info">
-          <h1>Payments infrastructure for the internet</h1>
+  return (
+    <section className='hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>
+            Payments infrastructure <br />
+            for the internet
+          </h1>
           <p>
-            Millions of companies if all sizes-form startups to 
-            Fortune 500s-use
-            Stripe's software and APIs to accept payments, send
-            payouts, and
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
             manage their businesses online.
           </p>
-          <button className="btn">
-            Start now
-          </button>
+          <button className='btn'>Start now</button>
         </article>
-        <article className="hero-images">
-          <img src={phoneImg} clssName="phone-img" alt="phone" />     
+        <article className='hero-images'>
+          <img src={phoneImg} className='phone-img' alt='phone' />
         </article>
       </div>
     </section>
   );
-}
+};
 
-export default Hero
+export default Hero;
--- src/context.js
+++ src/context.js
@@ -1,45 +1,48 @@
 import React, { useState, useContext } from 'react';
 import sublinks from './data';
-
 const AppContext = React.createContext();
 
-export const AppProvider = ({ children }) => {
-    const [ isSidebarOpen, setIsSidebarOpen ] = useState(false);
-    const [ isSubmenuOpen, setIsSubmenuOpen ] = useState(false);
-    const [ location, setLocation ] = useState({});
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
 
-    const openSidebar = () => {
-        setIsSidebarOpen(true);
-    }
-
-    const closeSidebar = () => {
-        setIsSidebarOpen(false);
-    }
-
-    const openSubmenu = ( text, coordinates ) => {
-        setLocation(coordinates);
-        setIsSubmenuOpen(true);
-    }
-
-    const closeSubmenu = () => {
-        setIsSubmenuOpen(false);
-    }
-
-    return(
-        <AppContext.Provider value={
-            isSidebarOpen,
-            isSubmenuOpen, 
-            openSidebar,
-            closeSidebar,
-            openSubmenu,
-            closeSubmenu,
-            location
-        }>
-            { children }
-        </AppContext.Provider>
-    );
-}
-
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
 export const useGlobalContext = () => {
-    return useContext(AppContext);
-}
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
